1069b605eae4a5a97b3530bdfcde9b1d052363cb
Sathvik307393 <307393@ust.com>
2026-02-18 10:16:16 +0000

    Disk Usage ALert Script done

--- Given Problem Statements/Disk_Usage_Alert/Disk_Usage_Alert.sh
+++ Given Problem Statements/Disk_Usage_Alert/Disk_Usage_Alert.sh
@@ -1,15 +1,12 @@
 #! /bin/bash
 
+THRESHOLD=40
+
 usage=$(df -hP / | awk 'NR==2 {gsub("%","",$5); print $5}')
 
 # Validate numeric value
-if [[ "$usage" =~ ^[0-9]+$ ]]; then
-    if [[ "$usage" -gt "$THRESHOLD" ]]; then
-        echo "High disk usage: ${usage}%"
-    else
-        echo "Disk usage is OK: ${usage}%"
-    fi
+if [[ "$usage" -gt "$THRESHOLD" ]]; then
+    echo "High disk usage: ${usage}%"
 else
-    echo "Error: Unable to determine disk usage."
-    exit 1
+    echo "Disk usage is OK: ${usage}%"
 fi
---
+++ Given Problem Statements/Disk_Usage_Alert/Readme.md
@@ -0,0 +1,11 @@
+# Shell Scripting on Disk Usage
+
+## 1. Setting up a threshold
+
+For example over here I have set the threshold value to 20 as the Disk is not being used more than 32%
+This threshold value helps us in evaluating whether the DISK is on HIGH USAGE or NORMAL USAGE
+
+## 2. Fetching exact disk usage at present
+
+Here the usage variable created fetches the present utilization of the disk and stores the value which
+will be later on used for checking if the usage is HIGH or MINIMAL
